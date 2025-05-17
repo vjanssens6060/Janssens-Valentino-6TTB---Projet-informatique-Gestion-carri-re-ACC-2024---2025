c42b0ec00bb6ce62a2d5ff90f93854cf33a5f6a3
vjanssens6060 <114067194+vjanssens6060@users.noreply.github.com>
2025-05-17 10:38:55 +0200

    Add files via upload

---
+++ projet_acc_2024_2025/test/main.py
@@ -0,0 +1,6 @@
+import ui  # Importation du module contenant la classe Application
+
+if __name__ == "__main__":
+    # Lancer l'application correctement
+    app = ui.Application()  # ✅ On crée une instance de l'application
+    app.mainloop()  # ✅ On démarre l'application avec la méthode correcte
